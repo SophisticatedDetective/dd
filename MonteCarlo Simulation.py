86c14f184ed5bfc2a48e8b2d7153b93581c5de78
Chen Du <38233705+SophisticatedDetective@users.noreply.github.com>
2018-12-30 13:44:10 +0800

    Create MonteCarlo Simulation.py

---
+++ MonteCarlo Simulation.py
@@ -0,0 +1,32 @@
+import tushare as ts
+sjht=ts.get_hist_data('002602')
+#sjht.shape
+#sjht.head()
+alter_sjht=sjht.sort_index(ascending=True)#按时间升序排列
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+t=1000
+s0=alter_sjht['close'][0]#初始股价
+#s0
+data=alter_sjht['close']#收盘价数据
+#data.mean()平均收盘价
+#Out[30]: 31.59659619450316
+return_on_data=data/data.shift(1)
+return_on_data-=1#收益率序列
+re=return_on_data.mean()#平均收益率
+#re
+#Out[35]: 0.0008092307033132518
+sigma=np.std(return_on_data)#收益率标准差
+#sigma
+#Out[37]: 0.031073640396856346
+n=2000
+dt=t/n
+d=(re-0.5*sigma**2)*dt#drift项
+K=np.zeros((n+1,1))
+K[0,0]=np.log(s0)
+for i in range(1,n+1):
+    K[i,0]=K[i-1,0]+d+sigma*np.sqrt(dt)*np.random.standard_normal()
+#matplotlib inline  #启用该项可以窗口内查看图片
+plt.plot(np.exp(K))
+#plt.gcf()
